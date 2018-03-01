eb1917d74a95475b44e9ab08b31b90b93ed3e0b7
Yann Le Du <ledu@kymasys.com>
2018-02-28 17:52:22 -0800

    MIPS: Do not disable LLSC CPU feature
    Update mips-virt to latest 0001-MIPS-Add-virtual-platform-linux-4.4.patch
    from l4re/l4/pkg/uvmm change ID:
    Change-Id: I6cabb709c9a618bf2daaba9bf2b49623dec15413

--- arch/mips/include/asm/mach-virt/cpu-feature-overrides.h
+++ arch/mips/include/asm/mach-virt/cpu-feature-overrides.h
@@ -9,7 +9,6 @@
 #define cpu_has_maar		0
 #define cpu_has_htw             0
 #define cpu_has_dc_aliases	1
-#define kernel_uses_llsc	0
 #define cpu_has_nan_legacy	1
 
 #endif /* __ASM_MACH_VIRT_CPU_FEATURE_OVERRIDES_H */
